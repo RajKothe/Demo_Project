534b8f75e9159241fed84b6d0aa0755a3ba62bf5
RajKothe <rajkothe3300@gmail.com>
2023-08-24 23:00:40 +0530

    my First file push

---
+++ src/nav.js
@@ -0,0 +1,10 @@
+// import { NavLink } from 'my-react-app'
+
+// export const Navbar = () => {
+//     return {
+//         <nav>
+//             <NavLink to = '/'>Home</NavLink>
+//             <NavLink to = '/'>About</NavLink>
+//         </nav>
+//     }
+// }
