14d09f70d1b27cf4619c9035e42d28e6264bb954
RajKothe <rajkothe3300@gmail.com>
2023-08-28 23:18:05 +0530

    todo file add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import logo from './logo.svg';
+ import React, {useState} from "react";
+ import ToDoLists from './ToDoLists';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [Items,setItems]= useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList]
+    });
+      setInputList("");
+  };
+
+  const deletItems = (id) => {
+    console.log("deleted");
+
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index)=> {
+          return index !== id; 
+      });
+    });
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hi Start React</h1>
-      </header>
+<>
+    <div className='main_div'>
+      <div className='center_div'>
+        <br />
+          <h1>To Do List</h1>
+        <br />
+        <input type='text' placeholder='Add a Items' onChange={itemEvent} />
+        <button onClick={listOfItems}> + </button>
+
+        <ol>     
+      {/* {   
+             Items.map((itemval) => {
+              return <li>{itemval}</li>
+     })
+     } */}
+
+
+          {
+          Items.map((itemval, index) => {
+            return <ToDoLists
+                     key={index} 
+                     id={index}
+                     text={itemval}
+                     onSelect={deletItems}
+                     />;
+          })} 
+
+
+      </ol>
+
+      </div>
     </div>
+</>
+
   );
 }
 
